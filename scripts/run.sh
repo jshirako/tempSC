0ddb83f81217211d8f44c11490922ee0dd535118
Youssef Elmougy <30897989+youssefelmougy@users.noreply.github.com>
2023-04-15 17:09:26 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -21,19 +21,20 @@ export HWPC=$hwpc
 
 # Run sbatch files corresponding to application version from  user
 if [ $application=="selector" ] ; then
-  echo "Running evaluations of the Selector application using $n cores and $N nodes"
+  echo "Running evaluations of the Selector application using $cores cores and $nodes nodes"
   #sbatch $PWD/scripts/jaccard_selector.sbatch $cores $nodes $hwpc
   sbatch -N $NODES -n $CORES --export=$HWPC $PWD/scripts/jaccard_selector.sbatch
 elif [ $application=="ctf" ] ; then
-  echo "Running evaluations of the GenomeAtScale application using $n cores and $N nodes"
+  echo "Running evaluations of the GenomeAtScale application using $cores cores and $nodes nodes"
   sbatch $PWD/scripts/jaccard_ctf.sbatch $cores $nodes $hwpc
 else
-  echo "Running evaluations of both the Selector application and the GenomeAtScale application using $n cores and $N nodes"
+  echo "Running evaluations of both the Selector application and the GenomeAtScale application using $cores cores and $nodes nodes"
   sbatch $PWD/scripts/jaccard_selector.sbatch $cores $nodes $hwpc
   sbatch $PWD/scripts/jaccard_ctf.sbatch $cores $nodes $hwpc
 fi
 
-echo "----------------------------------------------------------------------------------------------------------"
-echo "Selector and GenomeAtScale evaluations complete! Please see README for output interpretation instructions."
-echo "----------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------"
+echo "Job(s) submitted! Please check job status for completion and see README for output interpretation instructions."
+echo "---------------------------------------------------------------------------------------------------------------"
+
 
