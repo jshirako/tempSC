933c7129a966b4267bf8686eafe0791dff51d89c
Youssef Elmougy <30897989+youssefelmougy@users.noreply.github.com>
2023-04-15 19:02:53 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,36 +5,39 @@
 #                  -h [HWPC set: 0:none, 1:L1DA/L1DM/L1IA/L1IM, 2:L2DR/L2DM/L2IR/L2IM, 3:TLBDM/TLBIM, 4:BRINS/BRMSP, 5:INS/CYC]
 
 # Capture flags
-while getopts v:n:N: flag
-do
-    case "${flag}" in
-        v) application=${OPTARG};;
-        n) cores=${OPTARG};;
-        N) nodes=${OPTARG};;
-        h) hwpc=${OPTARG};;
-    esac
-done
+#while getopts v:n:h:N: flag
+#do
+#    case "${flag}" in
+#        v) application=${OPTARG};;
+#        n) cores=${OPTARG};;
+#        h) hw=${OPTARG};;
+#        N) nodes=${OPTARG};;
+#    esac
+#done
 
-export CORES=$cores
-export NODES=$nodes
-export HWPC=$hwpc
+#export CORES=$cores
+#export NODES=$nodes
+#export HWPC=$hw
+application=$1
+CORES=$2
+NODES=$3
+HWPC=$4
+echo $HWPC > hwpc.txt
 
 # Run sbatch files corresponding to application version from  user
-if [ $application=="selector" ] ; then
-  echo "Running evaluations of the Selector application using $cores cores and $nodes nodes"
-  #sbatch $PWD/scripts/jaccard_selector.sbatch $cores $nodes $hwpc
-  sbatch -N $NODES -n $CORES --export=$HWPC $PWD/scripts/jaccard_selector.sbatch
+if [ $application="selector" ] ; then
+  echo "Running evaluations of the Selector application using $CORES cores and $NODES nodes"
+  sbatch -N $NODES -n $CORES $PWD/scripts/jaccard_selector.sbatch
 elif [ $application=="ctf" ] ; then
-  echo "Running evaluations of the GenomeAtScale application using $cores cores and $nodes nodes"
-  sbatch $PWD/scripts/jaccard_ctf.sbatch $cores $nodes $hwpc
+  echo "Running evaluations of the GenomeAtScale application using $CORES cores and $NODES nodes"
+  sbatch -N $NODES -n $CORES $PWD/scripts/jaccard_ctf.sbatch
 else
-  echo "Running evaluations of both the Selector application and the GenomeAtScale application using $cores cores and $nodes nodes"
-  sbatch $PWD/scripts/jaccard_selector.sbatch $cores $nodes $hwpc
-  sbatch $PWD/scripts/jaccard_ctf.sbatch $cores $nodes $hwpc
+  echo "Running evaluations of both the Selector application and the GenomeAtScale application using $CORES cores and $NODES nodes"
+  sbatch -N $NODES -n $CORES $PWD/scripts/jaccard_selector.sbatch
+  sbatch -N $NODES -n $CORES $PWD/scripts/jaccard_ctf.sbatch
 fi
 
 echo "---------------------------------------------------------------------------------------------------------------"
 echo "Job(s) submitted! Please check job status for completion and see README for output interpretation instructions."
 echo "---------------------------------------------------------------------------------------------------------------"
-
 
