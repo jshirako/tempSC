683616afdb1bb7a73f3c1dcfc6f9d37c9dad10be
youssefelmougy <yelmougy1@pride.hofstra.edu>
2023-04-15 15:43:54 -0400

    First commit

---
+++ scripts/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# FORMAT: ./run.sh -v [version: selector, ctf, both] 
+#                  -n [# cores] -N [# nodes] 
+#                  -h [HWPC set: 0:none, 1:L1DA/L1DM/L1IA/L1IM, 2:L2DR/L2DM/L2IR/L2IM, 3:TLBDM/TLBIM, 4:BRINS/BRMSP, 5:INS/CYC]
+
+# Capture flags
+while getopts v:n:N: flag
+do
+    case "${flag}" in
+        v) application=${OPTARG};;
+        n) cores=${OPTARG};;
+        N) nodes=${OPTARG};;
+        h) hwpc=${OPTARG};;
+    esac
+done
+
+# Run sbatch files corresponding to application version from  user
+if [ $application=="selector" ] then
+  echo "Running evaluations of the Selector application using $n cores and $N nodes"
+  sbatch $PWD/scripts/jaccard_selector.sbatch $cores $nodes $hwpc
+elif [ $application=="ctf" ] then
+  echo "Running evaluations of the GenomeAtScale application using $n cores and $N nodes"
+  sbatch $PWD/scripts/jaccard_ctf.sbatch $cores $nodes $hwpc
+else 
+  echo "Running evaluations of both the Selector application and the GenomeAtScale application using $n cores and $N nodes"
+  sbatch $PWD/scripts/jaccard_selector.sbatch $cores $nodes $hwpc
+  sbatch $PWD/scripts/jaccard_ctf.sbatch $cores $nodes $hwpc
+fi
+
+echo "----------------------------------------------------------------------------------------------------------"
+echo "Selector and GenomeAtScale evaluations complete! Please see README for output interpretation instructions."
+echo "----------------------------------------------------------------------------------------------------------"
